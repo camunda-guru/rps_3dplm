4875538dfc5170dc190a0c30c98a48930b5c8b3e
eswaribala <Parameswaribala@gmail.com>
2017-10-03 15:58:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
-/*
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
 
-A promise can be:
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .usingServer('http://localhost:4444/wd/hub')
+    .build();
 
-fulfilled - The action relating to the promise succeeded
-rejected - The action relating to the promise failed
-pending - Hasn't fulfilled or rejected yet
-settled - Has fulfilled or rejected
-
-
- */
-
-function get(url) {
-    // Return a new promise.
-    return new Promise(function(resolve, reject) {
-        // Do the usual XHR stuff
-        var req = new XMLHttpRequest();
-        req.open('GET', url);
-
-        req.onload = function() {
-            // This is called even on 404 etc
-            // so check the status
-            if (req.status == 200) {
-                // Resolve the promise with the response text
-                resolve(req.response);
-            }
-            else {
-                // Otherwise reject with the status text
-                // which will hopefully be a meaningful error
-                reject(Error(req.statusText));
-            }
-        };
-
-        // Handle network errors
-        req.onerror = function() {
-            reject(Error("Network Error"));
-        };
-
-        // Make the request
-        req.send();
-    });
-}
-
-get('https://www.quandl.com/api/v3/datasets/OPEC/ORB.json').then(function(response) {
-    console.log("Success!", response);
-}, function(error) {
-    console.error("Failed!", error);
-})
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
